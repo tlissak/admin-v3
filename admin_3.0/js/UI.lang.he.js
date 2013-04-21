62be97e7bd17e4dbc07c5696966e52d2a13de60a
tlissak@gmail.com <tlissak@gmail.com@fd469863-29af-7a11-4273-52d2e58bee00>
2013-04-21 00:50:58 +0000

    better lang support

---
+++ admin_3.0/js/UI.lang.he.js
@@ -0,0 +1,39 @@
+(function(ui){
+	ui.lang = {
+			CONFIRM_ITEM_SUPPRESSION : 'אתה בטוח שאתה רוצה למחוק?'
+			,CONFIRM_ITEM_DUPPLICATION : 'אתה בטוח שאתה רוצה להעתיק ?' 			
+			,POST_BACK_MODIFIED 		:' שונה בהצלחה'
+			,POST_BACK_DELETED 		:' נמחק בהצלחה'
+			,POST_BACK_ADDED 			:' נשמר בהצלחה'
+			,POST_BACK_DUPLICATED 	:' הועתק בהצלחה'			
+			,VALIDATION_DATA_REQUIRED:'שדה חובה' 
+			,VALIDATION_DATA_LIMIT : 'יותר מדי כיתוב'			
+			,CONFIRM_DATA_LOST:'===========\n -- זהירות -- \n נתונים שהוכנסו ימחקו אתה רוצה להמשיך ?'
+			,UNVALID_URL:'לינק שגוי'
+			,UNVALID_LINK:'לינק שגוי'
+			,UNVALID_INTEGER:'מספר שגוי'
+			,UNVALID_FLOAT:'ספרה שגויה'
+			,UNVALID_TEXT:'כיתוב שגוי'
+			,UNVALID_ZIPCODE : 'מיקוד שגוי'
+			,UNVALID_EMAIL :'אימייל שגוי'
+			,UNVALID_PHONE :'טלפון שגוי'
+			,UNVALID_PASSWORD_PATTERN :'Mot de passe non valide (7-16 charc. chifre, _ , commencer par une lettre)'
+			,UNVALID_DATE_FORMAT : 'תאריך שגוי (ex: jj/mm/aaaa ou jj-mm-aaaa)'
+			,UNVALID_COLOR_FORMAT : 'צבע שגוי (ex: #AAA ou #A1C1F9)'
+			
+			,ERROR_DELTING_FILE:'שגיאה במחיקת הקובץ'
+			
+			,FILE_UPLOAD:{
+					"BrowserNotSupported" : 'Navigateur non compatible'
+					,"TooManyFiles":'Nombre de fichiers trop elever'
+					 ,"FileTooLarge":'Fichier trop grande'
+					 ,"FileTypeNotAllowed":'Type de fichier interdit'
+					 ,"NotFound":'[FU_404] Fichier non trouver'
+					 , "NotReadable":'[FU_304] Fichier non accessible'
+					 , "AbortError":'[FU_577] Erreur '
+					 , "ReadError" : '[FU_578] Erreur '
+			}
+			
+			
+	}	
+}(UI))

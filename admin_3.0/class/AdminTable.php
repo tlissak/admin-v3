2dfe94685e6162b6d0d7725f72e1c1835cbf3d59
tlissak@gmail.com <tlissak@gmail.com@fd469863-29af-7a11-4273-52d2e58bee00>
2013-05-31 13:03:48 +0000

    fixed :jquery non submit unchecked checkbox issue    form.serialize is now serializeArray ( also in feature will serve to submit only the changes that made) many french lang fixs (in js/controller ) ui.log added debug post action when error accord AdminTable::debug to trace posted data

--- admin_3.0/class/AdminTable.php
+++ admin_3.0/class/AdminTable.php
@@ -1,10 +1,13 @@
 <?
 class AdminTable extends AdminRelation {
-
+	
+	public $debug = false;
+	
 	public $db = null;
 	
 	public $name = '';
 	public $fields = array();
+	public $fields_pairs = array();
 	
 	public $data = array();	
 	public $post_data = array();	
@@ -17,7 +20,10 @@ class AdminTable extends AdminRelation {
 	 * will initialise the ->post_data array property with the data posted 
 	 * Important !!! will initialise only the submited data
 	 */
-	function initPostData(){	foreach($this->fields as $k){	if (isset($_POST[$k])) { $this->post_data[$k]	 = post($k);	}}} 
+	function initPostData(){ 
+		foreach($this->fields as $k){	if (isset($_POST[$k])) { $this->post_data[$k]	 = post($k);	}}
+		//Observe to check more unsbmited data !
+		if ($this->debug) { fb('fields , fields_pairs, post data   , post_data  :');fb($this->fields); fb($this->fields_pairs); fb($_POST); fb($this->post_data) ; }} 
 	/**
 	 * initialise the $data property to data from the database
 	 * @return
@@ -42,7 +48,8 @@ class AdminTable extends AdminRelation {
 		$this->db = $db ; // should use & by ref for performences ?
 		$this->id 		=  (int)(post('id')) ? (int)(post('id')) :   (int)(get('id'));	 
 		$this->name = $name ;
-		$this->fields = array_filter(array_keys( $this->db->ctypes( $this->name ) ), create_function('$kyes','return $kyes!="id";')) ;
+		$this->fields_pairs = $this->db->ctypes( $this->name ) ;
+		$this->fields = array_filter(array_keys($this->fields_pairs  ), create_function('$kyes','return $kyes!="id";')) ;
 	}
 	/**
 	 * Will Add submited row to the database
@@ -51,6 +58,7 @@ class AdminTable extends AdminRelation {
 	public function Add(){
 		$this->initPostData() ;	
 		$this->db->q(SQL::build('INSERT',$this->name,$this->post_data) ) ;
+		if(count($this->db->errors)){ fb('Post add db error ', $this->db->last_error);}
 		$this->id = $this->db->last_id() ;
 		$this->deleteRelations() ;
 		$this->addRelations() ;	
@@ -63,6 +71,7 @@ class AdminTable extends AdminRelation {
 		$this->initData(); //empty for fields keys only
 		$this->initDbRelationData() ;
 		$this->db->q(SQL::build('DUPLICATE',$this->name,$this->data,$this->id) ) ;
+		if(count($this->db->errors)){ fb('Post duplicate db error ', $this->db->last_error);}
 		$this->id = $this->db->last_id() ;		
 		$this->addRelations( true ) ;
 	}
@@ -75,6 +84,7 @@ class AdminTable extends AdminRelation {
 		$this->initPostData() ;	
 		if ($this->id < 0) return ; 
 		$this->db->q(SQL::build('UPDATE',$this->name,$this->post_data,$this->id)) ; 	
+		if(count($this->db->errors)){ fb('Post edit db error ', $this->db->last_error);}
 		$this->deleteRelations() ;
 		$this->addRelations() ;
 	}	
@@ -83,6 +93,7 @@ class AdminTable extends AdminRelation {
 	 */
 	public function Delete(){
 		$this->db->q('DELETE  FROM `'.$this->name.'` WHERE id = '. $this->id) ;
+		if(count($this->db->errors)){ fb('Post delete db error ', $this->db->last_error);}
 		$this->deleteRelations() ; 
 		$this->id =  0 ; 
 	}

7ef24ff23f38970888ebeb572bece0ed18df98c1
tlissak@gmail.com <tlissak@gmail.com@fd469863-29af-7a11-4273-52d2e58bee00>
2013-11-28 11:07:38 +0000

    see previus commit !

--- admin_3.0/modules/filebrowser/UI.fileBrowser.js
+++ admin_3.0/modules/filebrowser/UI.fileBrowser.js
@@ -44,8 +44,20 @@
 	})
 	
 	ui.formLoad(function(){
+		function bindFirst(elm,name, fn) { 
+				elm.bind(name, fn);
+				var handlers = $._data(elm.get(0), "events")[name.split('.')[0]] ;	
+				var handler = handlers.pop();
+				handlers.splice(0, 0, handler);
+		};
 		
-		$('.rte-zone .rte-toolbar .image').unbind('click').bind('click',function(e){
+		bindFirst($('.rte-zone .rte-toolbar .image'),'click',function(e){
+			
+			//override the click event
+			e.stopImmediatePropagation();
+			e.stopPropagation();  
+			e.preventDefault();
+		
 			UI.current_iframe = $(this).closest('.rte-zone').find('>iframe')[0] ;
 			UI.browseFilesCallback = function(a){				
 					var URI = $(this).attr('href').replace('#',"");
